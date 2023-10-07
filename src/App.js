506fc87c86137d2aabc21907f325657e8268188d
omar-syed <omarrrrr118@gmail.com>
2023-10-07 22:00:03 +0300

    Final Edit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,4 @@
-// import Header from "./components/Header";
-// import "bootstrap/dist/css/bootstrap.css";
-// import "./App.css";
-// import Home from "./components/Home";
-// import { Route, Routes, BrowserRouter } from "react-router-dom";
-// import WatchList from "./components/Watchlist";
-// import Search from "./components/Search";
-// import MovieDetail from "./components/MovieDetail";
-// import NotFound from "./components/NotFound";
-// function App() {
-//   return (
-//     <div className="App">
-//       <BrowserRouter>
-//         <Header />
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/movie-detail/:id" element={<MovieDetail />} />
-//           <Route path="/watchlist" element={<WatchList />} />
-//           <Route path="/search/:title" element={<Search />} />
-//           <Route path="/404" element={<NotFound />} />
-//           <Route path="/*" element={<NotFound />} />
-//         </Routes>
-//       </BrowserRouter>
-//     </div>
-//   );
-// }
 
-// export default App;
 import Header from "./components/Header";
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.css";
