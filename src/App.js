119041f3d80cc8aeab7ff70293da9b2a4cf84cb6
Omar elsayed <71873207+omar-syed@users.noreply.github.com>
2023-10-10 22:12:11 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,3 @@
-// import Header from "./components/Header";
-// import "bootstrap/dist/css/bootstrap.css";
-// import "./App.css";
-// import Home from "./components/Home";
-// import { Route, Routes, BrowserRouter } from "react-router-dom";
-// import WatchList from "./components/Watchlist";
-// import Search from "./components/Search";
-// import MovieDetail from "./components/MovieDetail";
-// import NotFound from "./components/NotFound";
-// function App() {
-//   return (
-//     <div className="App">
-//       <BrowserRouter>
-//         <Header />
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/movie-detail/:id" element={<MovieDetail />} />
-//           <Route path="/watchlist" element={<WatchList />} />
-//           <Route path="/search/:title" element={<Search />} />
-//           <Route path="/404" element={<NotFound />} />
-//           <Route path="/*" element={<NotFound />} />
-//         </Routes>
-//       </BrowserRouter>
-//     </div>
-//   );
-// }
-
-// export default App;
 import Header from "./components/Header";
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.css";
@@ -47,7 +19,7 @@ import SignIn from "./components/SignIn";
 // import NotFound from "./components/NotFound";
 const Home =React.lazy(() => import('./components/Home'));
 const MovieDetail =React.lazy(() => import('./components/MovieDetail'));
-const WatchList =React.lazy(() => import('./components/WatchList'));
+const WatchList =React.lazy(() => import('./components/Watchlist'));
 const Search =React.lazy(() => import('./components/Search'));
 const NotFound =React.lazy(() => import('./components/NotFound'));
 
@@ -72,7 +44,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/movie-detail/:id" element={<MovieDetail />} />
-            <Route path="/WatchList" element={<WatchList />} />
+            <Route path="/Watchlist" element={<WatchList />} />
             <Route path="/search/:title" element={<Search />} />
             <Route path="/404" element={<NotFound />} />
             <Route path="/new-User" element={<NewUser />} />
