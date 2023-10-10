f0bc30076daedf434648b47a251b0b83aa52aff4
mohammedaliem <Muhammed.alyaleem@gmail.com>
2023-10-10 21:31:53 +0300

    add context singup signin

--- src/App.js
+++ src/App.js
@@ -33,6 +33,13 @@ import "./App.css";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Loader from "./components/Loader/Loader";
 import { Suspense } from "react";
+import { LanguageContext } from "./context/language";
+import { ThemeContext } from "./context/theme";
+import { useContext, useState } from "react";
+import NewUser from "./components/NewUser";
+import SignIn from "./components/SignIn";
+
+
 // import Home from "./components/Home";
 // import WatchList from "./components/Watchlist";
 // import Search from "./components/Search";
@@ -44,11 +51,23 @@ const WatchList =React.lazy(() => import('./components/WatchList'));
 const Search =React.lazy(() => import('./components/Search'));
 const NotFound =React.lazy(() => import('./components/NotFound'));
 
+
 function App() {
+  const [contextLang, setContextLang] = useState("en");
+  const [contextTheme, setContextTheme] = useState("light");
+
+
   return (
-    <div className="App">
+  <div className={contextTheme === "light" ? "App" : "App bg-dark text-white"}>
+    <div className="App" 
+        dir={contextLang === "en" ? "ltr" : "rtl"}>
       <BrowserRouter>
+          <ThemeContext.Provider value={{ contextTheme, setContextTheme }}>
+
+              <LanguageContext.Provider value={{ contextLang, setContextLang }}>
+
         <Header />
+
         <Suspense fallback={<Loader />}>
           <Routes>
             <Route path="/" element={<Home />} />
@@ -56,11 +75,18 @@ function App() {
             <Route path="/WatchList" element={<WatchList />} />
             <Route path="/search/:title" element={<Search />} />
             <Route path="/404" element={<NotFound />} />
+            <Route path="/new-User" element={<NewUser />} />
+            <Route path="/sign-in" element={<SignIn />} />
+
+
             <Route path="/*" element={<NotFound />} />
           </Routes>
         </Suspense>
+        </LanguageContext.Provider>
+      </ThemeContext.Provider>
       </BrowserRouter>
     </div>
+  </div>
   );
 }
 
---
+++ src/components/SignIn/index.js
@@ -0,0 +1,148 @@
+import React, { useState } from "react";
+
+export default function SignIn() {
+  const [userForm, setUserForm] = useState({
+    name: "",
+    email: "",
+    userName: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [userFormError, setUserFormError] = useState({
+    name: null,
+    email: null,
+    userName: null,
+    password: null,
+  });
+  const handleChange = (e) => {
+    if (e.target.name === "name") {
+      setUserForm({ ...userForm, name: e.target.value });
+      if (e.target.value === "") {
+        setUserFormError({ ...userFormError, name: "Name is required" });
+      } else setUserFormError({ ...userFormError, name: null });
+    }
+    if (e.target.name === "email") {
+      setUserForm({ ...userForm, email: e.target.value });
+      if (e.target.value === "") {
+        setUserFormError({ ...userFormError, email: "Email is required" });
+      } else if (
+        !e.target.value.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/)
+      ) {
+        setUserFormError({ ...userFormError, email: "Not a valid Email " });
+      } else setUserFormError({ ...userFormError, email: null });
+    }
+    if (e.target.name === "userName") {
+      setUserForm({ ...userForm, userName: e.target.value });
+      if (e.target.value === "") {
+        setUserFormError({
+          ...userFormError,
+          userName: "User Name is required",
+        });
+      } else if (!e.target.value.match(/^\S+$/)) {
+        setUserFormError({
+          ...userFormError,
+          userName: "User Name cannot have whitespace characters.",
+        });
+      } else setUserFormError({ ...userFormError, userName: null });
+    }
+    if (e.target.name === "password") {
+      setUserForm({ ...userForm, password: e.target.value });
+      if (e.target.value === "") {
+        setUserFormError({
+          ...userFormError,
+          password: "Password is required",
+        });
+      } else if (
+        !e.target.value.match(
+          /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/
+        )
+      ) {
+        setUserFormError({
+          ...userFormError,
+          password:
+            "Password must be at least 8 characters long containing one digit, one lowercase character, one uppercase character and a special character",
+        });
+      } else setUserFormError({ ...userFormError, password: null });
+    }    
+      if (e.target.name === "confirmPassword") {
+        setUserForm({ ...userForm, confirmPassword: e.target.value });
+        if (e.target.value !== userForm.password) {
+          setUserFormError({
+            ...userFormError,
+            confirmPassword: "Passwords do not match",
+          });
+        } else {
+          setUserFormError({ ...userFormError, confirmPassword: null });
+        }
+      }
+    
+    
+  };
+  const preventSubmit = (e) => {
+    e.preventDefault();
+    console.log(userForm);
+  };
+
+  return (
+    <div className="container">
+      <form onSubmit={preventSubmit}>
+        <div className="mb-3">
+
+          <div id="nameHelp" className="form-text">
+            {userFormError.name}
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input
+            name="email"
+            className="form-control"
+            id="email"
+            value={userForm.email}
+            onChange={handleChange}
+          />
+          <div id="emailHelp" className="form-text">
+            {userFormError.email}
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="userName" className="form-label">
+            User Name
+          </label>
+          <input
+            className="form-control"
+            id="userName"
+            name="userName"
+            value={userForm.userName}
+            onChange={handleChange}
+          />
+          <div id="userNameHelp" className="form-text">
+            {userFormError.userName}
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            className="form-control"
+            id="password"
+            value={userForm.password}
+            onChange={handleChange}
+          />
+ 
+
+      
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          log in
+        </button>
+      </form>
+    </div>
+  );
+}
